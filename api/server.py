2e80179b2ef7294db021f38bbe5e063887c19fe3
Hairum Qureshi <hqureshi@udel.edu>
2026-02-23 02:22:14 -0500

    configured Magnum

--- api/server.py
+++ api/server.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
+from mangum import Mangum
 
 app = FastAPI()
-model = SentenceTransformer('all-MiniLM-L6-v2')
+handler = Mangum(app)  # <-- THIS LINE MAKES IT WORK
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 class TextRequest(BaseModel):
     id: str
@@ -14,15 +17,16 @@ class EmbeddingResponse(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the text embedding API. Use the /embed endpoint to get embeddings for your text."}
+    return {
+        "message": "Welcome to the text embedding API. Use the /embed endpoint."
+    }
 
 @app.post("/embed")
 def embed_text(req: TextRequest):
     embedding = model.encode(req.text).tolist()
     return {"id": req.id, "embedding": embedding}
 
-
-@app.post('/query-to-embedding')
+@app.post("/query-to-embedding")
 def query_to_embedding(req: EmbeddingResponse):
     embedding = model.encode(req.text).tolist()
-    return { "embedding": embedding}
+    return {"embedding": embedding}
